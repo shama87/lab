61f25034474505c4e7ac02c123f46d014e0028f7
Shama Zerin <nahin1757@gmail.com>
2025-11-02 13:08:55 +0600

    Add files via upload

---
+++ task 5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    int sum = 0;
+
+    cout << "Enter " << n << " elements:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    float average = (float)sum / n;
+    cout << "Average value: " << average << endl;
+
+    return 0;
+}
